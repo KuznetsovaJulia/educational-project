fb8cb683fa976c67ab1ce3842353794a3a7df424
KuznetsovaJulia <julia-kuz95@yandex.ru>
2018-02-28 15:36:18 +0300

    dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  unauthenticated do
+    root :to => 'static_pages#home'
+  end
+  authenticated do
+    root :to => 'dashboard#index'
+  end
+  get '/home' => 'static_pages#home', as: 'helf'
+  get '/help'=> 'static_pages#help', as: :help
+  get '/about' => 'static_pages#about', as: :about
+
+  devise_for :users,
+             :path_names => {
+                     :sign_in => 'login',
+                     :sign_out => 'logout',
+                     :password => 'secret',
+                     :confirmation => 'verification',
+                     :unlock => 'unblock',
+                     :registration => 'register',
+                     :sign_up => 'cmon_let_me_in' }
 end
