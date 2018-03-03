dacc2b35005be4270a63585b17e3b82503b6965c
KuznetsovaJulia <julia-kuz95@yandex.ru>
2018-03-03 23:51:53 +0300

    category

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,6 @@ class Course < ApplicationRecord
     has_many :subscriptions, dependent: :destroy
     has_many :users, through: :subscriptions
     belongs_to :author, :class_name => 'User', :foreign_key => 'author_id', optional: true
+    has_many :categorization
+    has_many :categories, through: :categorization
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180303151519) do
+ActiveRecord::Schema.define(version: 20180304105949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categorizations", force: :cascade do |t|
+    t.bigint "course_id"
+    t.bigint "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_categorizations_on_category_id"
+    t.index ["course_id"], name: "index_categorizations_on_course_id"
+  end
 
   create_table "courses", force: :cascade do |t|
     t.string "name"
@@ -54,4 +69,6 @@ ActiveRecord::Schema.define(version: 20180303151519) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categorizations", "categories"
+  add_foreign_key "categorizations", "courses"
 end
